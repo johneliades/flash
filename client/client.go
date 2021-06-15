ac18f7356d8443c17623186d9b714d7b73838e78
johneliades <36938242+johneliades@users.noreply.github.com>
2021-06-16 00:34:44 +0300

    defined messages

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/johneliades/flash_torrent/handshake"
 	"github.com/johneliades/flash_torrent/peer"
+	"github.com/johneliades/flash_torrent/message"
 	"net"
 	"time"
 )
@@ -43,30 +44,24 @@ func New(peer peer.Peer, peerID, infoHash [20]byte) (*Client, error) {
 		return nil, fmt.Errorf("Expected infohash %x but got %x", res.InfoHash, infoHash)
 	}
 
-	//	conn.SetDeadline(time.Now().Add(5 * time.Second))
-	//	defer conn.SetDeadline(time.Time{})
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	defer conn.SetDeadline(time.Time{})
 
-	/*
-		msg, ok := message.Read(conn)
-		if ok != nil {
-			panic(ok)
-		}
+	msg, ok := message.Read(conn)
+	if ok != nil {
+		return &Client{}, ok
+	}
 
-		if msg.ID != message.MsgbitField {
-			err := fmt.Errorf("Expected bitField but got ID %d", msg.ID)
-			return nil, err
-		}
+	if msg.ID != message.BitField {
+		return nil, fmt.Errorf("Expected bitField's id but got: %d", msg.ID)
+	}
 
-		bf = msg.Payload
-
-		return &Client{
-			conn:     conn,
-			choked:   true,
-			bitField: []byte,
-			peer:     peer,
-			infoHash: infoHash,
-			peerID:   peerID,
-		}, nil */
-
-	return nil, ok
+	return &Client{
+		conn:     conn,
+		choked:   true,
+		bitField: msg.Payload,
+		peer:     peer,
+		infoHash: infoHash,
+		peerID:   peerID,
+	}, nil
 }
--- message/message.go
+++ message/message.go
@@ -1,1 +1,64 @@
 package message
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+const (
+	choke         uint8 = 0
+	unchoke       uint8 = 1
+	interested    uint8 = 2
+	notInterested uint8 = 3
+	have          uint8 = 4
+	BitField      uint8 = 5
+	request       uint8 = 6
+	piece         uint8 = 7
+	cancel        uint8 = 8
+)
+
+type Message struct {
+	ID      uint8
+	Payload []byte
+}
+
+// <length prefix><message ID><payload>
+func (message *Message) Serialize() []byte {
+	if message == nil {
+		return make([]byte, 4)
+	}
+
+	length := uint32(len(message.Payload) + 1) // +1 for id
+
+	buf := make([]byte, 4+length)
+	binary.BigEndian.PutUint32(buf[0:4], length)
+	buf[4] = byte(message.ID)
+	copy(buf[5:], message.Payload)
+
+	return buf
+}
+
+func Read(reader io.Reader) (*Message, error) {
+	lengthBuf := make([]byte, 4)
+	_, err := io.ReadFull(reader, lengthBuf)
+	if err != nil {
+		return nil, err
+	}
+	length := binary.BigEndian.Uint32(lengthBuf)
+
+	// keep-alive message
+	if length == 0 {
+		return nil, nil
+	}
+
+	messageBuf := make([]byte, length)
+	_, err = io.ReadFull(reader, messageBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Message{
+		ID:      uint8(messageBuf[0]),
+		Payload: messageBuf[1:],
+	}, nil
+}
